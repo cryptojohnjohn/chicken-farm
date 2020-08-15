5b9277bcb77887494ccd8293e49e7668291aeca0
CryptoJohnJohn <soulbar@protonmail.com>
2020-08-15 06:38:56 +0200

    added missing pepe resources.

---
+++ yam-www/src/components/CardIcon/components/GBPCardIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import CardIcon from '../CardIcon'
+
+import pepe from '../../../assets/img/pepe.png'
+
+const GBPCardIcon: React.FC = () => (
+  <CardIcon><img src={pepe} height="36" /></CardIcon>
+)
+
+export default GBPCardIcon
